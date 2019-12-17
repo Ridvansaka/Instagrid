81428229d5771693f338489ba7d4bd45b7f3b947
Ridvan SAKA <ridvansaka@gmail.com>
2019-12-17 23:12:15 +0300

    Initial Commit

---
+++ Instagrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by SAKA Ridvan on 17/12/2019.
+//  Copyright © 2019 SAKA Ridvan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
