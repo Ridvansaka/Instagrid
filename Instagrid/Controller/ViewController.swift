56c0ca3bb4974cde249cca4ca453a1c4b3e18527
Ridvan SAKA <ridvansaka@gmail.com>
2019-12-20 23:18:26 +0300

    Added function for Create views on stackView according to layout model

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,25 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var swipeUpLabel: UILabel!
     @IBOutlet weak var firstButtonView: UIView!
+    @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var bottomStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    private func createViewsAccordingTo(layout: Layout) {
+        //        cleanGrid()
+        addViewsToStackView(stackView: topStackView, number: layout.getTopNumber())
+        addViewsToStackView(stackView: bottomStackView, number: layout.getBottomNumber())
+    }
+    
+    private func addViewsToStackView(stackView: UIStackView, number: Int) {
+        for _ in 1...number {
+            let button = UIButton()
+            button.backgroundColor = .black
+            stackView.addArrangedSubview(button)
+        }
+    }
+    
     @IBAction func didTapFirstButton() {
-         firstButtonView.isHidden = false
+        firstButtonView.isHidden = false
+        createViewsAccordingTo(layout: .OneTopTwoBottom)
     }
     
     @IBAction func didTapSecondButton() {
-        firstButtonView.isHidden = true
+        firstButtonView.isHidden = false
+        createViewsAccordingTo(layout: .TwoTopOneBottom)
     }
     
     @IBAction func didTapThirdButton() {
         firstButtonView.isHidden = false
+        createViewsAccordingTo(layout: .TwoTopTwoBottom)
+    }
+    
+    private func cleanGrid() {
+//        for view in topStackView.arrangedSubviews {
+//            view.removeFromSuperview()
+//        }
+//        for view in bottomStackView.arrangedSubviews {
+//            view.removeFromSuperview()
+//        }
     }
 }
 
---
+++ Instagrid/Model/Layout.swift
@@ -0,0 +1,33 @@
+//
+//  Layout.swift
+//  Instagrid
+//
+//  Created by SAKA Ridvan on 20/12/2019.
+//  Copyright © 2019 SAKA Ridvan. All rights reserved.
+//
+
+import Foundation
+enum Layout {
+    case OneTopTwoBottom, TwoTopOneBottom, TwoTopTwoBottom
+    
+    func getTopNumber() -> Int {
+        switch self {
+        case .OneTopTwoBottom:
+            return 1
+        case .TwoTopOneBottom:
+            return 2
+        case .TwoTopTwoBottom:
+            return 2
+        }
+    }
+    func getBottomNumber() -> Int {
+        switch self {
+        case .OneTopTwoBottom:
+            return 2
+        case .TwoTopOneBottom:
+            return 1
+        case .TwoTopTwoBottom:
+            return 2
+        }
+    }
+}
