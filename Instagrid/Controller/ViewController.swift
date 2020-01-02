8eb1a41ee8c1015c118b789d04ce1775622009f8
Ridvan SAKA <ridvansaka@gmail.com>
2020-01-02 20:39:22 +0300

    Completed saving photo layout on swipe

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var topStackView: UIStackView!
     @IBOutlet weak var bottomStackView: UIStackView!
     
+    var selectedImageButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,21 +24,55 @@ class ViewController: UIViewController {
     
     private func createViewsAccordingTo(layout: Layout) {
         cleanGrid()
-        addViewsToStackView(stackView: topStackView, number: layout.getTopNumber())
-        addViewsToStackView(stackView: bottomStackView, number: layout.getBottomNumber())
+        addViewsToStackView(stackView: topStackView, numberOfView: layout.getTopNumber())
+        addViewsToStackView(stackView: bottomStackView, numberOfView: layout.getBottomNumber())
     }
     
-    private func addViewsToStackView(stackView: UIStackView, number: Int) {
-        for _ in 1...number {
+    private func addViewsToStackView(stackView: UIStackView, numberOfView: Int) {
+        for _ in 1...numberOfView {
             let button = UIButton()
             button.backgroundColor = .white
             button.setImage(UIImage(named: "Plus"), for: .normal)
-            button.addTarget(self, action: #selector(didTapOnPhotoButton), for: .touchUpInside )
+            button.addTarget(self, action: #selector(didTapOnPhotoButton(sender:)), for: .touchUpInside)
             stackView.addArrangedSubview(button)
         }
     }
     
-    @objc private func didTapOnPhotoButton() {
+    @objc private func didTapOnPhotoButton(sender: UIButton) {
+
+        selectedImageButton = sender
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: {
+        })
+    }
+    
+    
+    @IBAction func didSwipeOnMainView(_ sender: UISwipeGestureRecognizer) {
+     
+        //let image = UIImage(named: "test-image")
+        let image = gridView.asImage()
+        let items = [image]
+        let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        present(ac, animated: true)
+        
+        
+//        let activityController = UIActivityViewController (activityItems: [gridView!], applicationActivities: [])
+//
+//        self.present ( activityController , animated : true , completion: nil )
+        
+        
+//        if let image = gridView {
+//            let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+//             present(vc, animated: true, completion: nil)
+//        }
+        
+        
+//        let activityController = UIActivityViewController()
+//        present(activityController, animated: true, completion: {
+//            
+//        })
     }
     
     @IBAction func didTapFirstButton() {
@@ -56,11 +92,43 @@ class ViewController: UIViewController {
     
     private func cleanGrid() {
         for view in topStackView.arrangedSubviews {
-            topStackView.removeArrangedSubview(view)
+            view.removeFromSuperview()
         }
         for view in bottomStackView.arrangedSubviews {
-            bottomStackView.removeArrangedSubview(view)
+            view.removeFromSuperview()
         }
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            if let button = selectedImageButton {
+                button.imageView?.contentMode = .scaleAspectFill
+                button.setImage(pickedImage, for: .normal)
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
+}
+
+extension UIView {
+
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
